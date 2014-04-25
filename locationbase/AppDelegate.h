514dac1dd8bf1fe32a3aa754211662388381c9c9
Rodney Degracia <rdegraci@elvenlabs.com>
2014-04-24 23:52:32 -0400

    Initial Commit

---
+++ locationbase/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  locationbase
+//
+//  Created by Rodney Degracia on 4/22/14.
+//  Copyright (c) 2014 Venture Intellectual LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ locationbase/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  locationbase
+//
+//  Created by Rodney Degracia on 4/22/14.
+//  Copyright (c) 2014 Venture Intellectual LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
